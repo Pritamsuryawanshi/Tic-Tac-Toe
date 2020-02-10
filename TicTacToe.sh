fd36c1b2ba3fb0b453d51abe50712fe8281a1d41
Pritamsuryawanshi <pritam.surya110@gmail.com>
2020-02-10 09:55:17 +0530

    ADDED: A function to reset the board and start fresh

--- TicTacToe.sh
+++ TicTacToe.sh
@@ -1,2 +1,20 @@
 #1/bin/bash
 echo "Welcome to the tic-tac-toe game "
+
+#Tic-Tac-Toe array declaration
+tic=(- - - - - - - - - -)
+
+#function to reset the board
+function resetTheBoard()
+{
+echo "--------------"
+echo "  ${tic[1]} | ${tic[2]} | ${tic[3]}"
+echo "--------------"
+echo "  ${tic[4]} | ${tic[5]} | ${tic[6]}"
+echo "--------------"
+echo "  ${tic[7]} | ${tic[8]} | ${tic[9]}"
+echo "--------------"
+}
+
+#MAIN
+
