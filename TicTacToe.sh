e9f8bf413dbb60ad03f8432d64e43635a0a842ce
Pritamsuryawanshi <pritam.surya110@gmail.com>
2020-02-11 10:01:57 +0530

    ADDED: Condition of winning and tie

--- TicTacToe.sh
+++ TicTacToe.sh
@@ -1,18 +1,21 @@
-#1/bin/bash
+#!/bin/bash 
 echo "Welcome to the tic-tac-toe game "
 
+#VARIABLES
+count=0
+
 #Tic-Tac-Toe array declaration
-tic=(- - - - - - - - - -)
+gameBoard=(- - - - - - - - - -)
 
 #Function to reset the board
-function resetTheBoard()
+function displayTheBoard()
 {
 	echo "--------------"
-	echo "  ${tic[1]} | ${tic[2]} | ${tic[3]}"
+	echo "  ${gameBoard[1]} | ${gameBoard[2]} | ${gameBoard[3]}"
 	echo "--------------"
-	echo "  ${tic[4]} | ${tic[5]} | ${tic[6]}"
+	echo "  ${gameBoard[4]} | ${gameBoard[5]} | ${gameBoard[6]}"
 	echo "--------------"
-	echo "  ${tic[7]} | ${tic[8]} | ${tic[9]}"
+	echo "  ${gameBoard[7]} | ${gameBoard[8]} | ${gameBoard[9]}"
 	echo "--------------"
 }
 
@@ -49,6 +52,91 @@ function tossTheCoin()
 	fi
 }
 
-#MAIN
+#Function to check if the place is empty or occupied
+function isEmpty()
+{
+	if [[ ${gameBoard[$positionNumber]} == "-" ]]
+	then
+		gameBoard[$positionNumber]="$1"
+		((count++))
+		echo "count is $count"
+	else
+		echo "it's already occupied choose another one"
+	fi
+	displayTheBoard
+}
 
-resetTheBoard
+#Function to play the game
+function play()
+{
+	while [ count != 10 ]
+	do
+		read -p "enter the position to tick: " positionNumber
+		isEmpty "X"
+		if (( count > 2 ))
+		then
+			winningCondition
+		fi
+	done
+
+}
+
+#Function to check winning condition for rows
+function winningConditionForRows()
+{
+if [[ "${gameBoard[$i]}" == "${gameBoard[$i+1]}" &&  "${gameBoard[$i]}" == "${gameBoard[$i+2]}" ]] && [[ "${gameBoard[$i]}" != "-" ]] && (( i == 1 || i == 4 || i == 7 ))
+	then
+		echo "You've won"
+		exit
+	fi
+}
+
+
+#Function to check winning condition for rows
+function winningConditionForColumns()
+{
+	if [[ "${gameBoard[$i]}" == "${gameBoard[$i+3]}" && "${gameBoard[$i]}" == "${gameBoard[$i+6]}" ]] && [[ "${gameBoard[$i]}" != "-" ]] && (( i>=1 && i<=3 ))
+	then
+		echo "You've won"
+		exit
+	fi
+}
+#Function to check winning condition for Diagonals
+function winningConditionForDiagonals()
+{
+	if [[ "${gameBoard[1]}" == "${gameBoard[5]}" ]] && [[ "${gameBoard[5]}" == "${gameBoard[9]}" ]] && [[ "${gameBoard[1]}" != "-" ]]
+	then
+		echo "You've won"
+		exit
+	elif [[ "${gameBoard[3]}" == "${gameBoard[5]}" ]] && [[ "${gameBoard[5]}" == "${gameBoard[7]}" ]] && [[ "${gameBoard[3]}" != "-" ]]
+	then
+		echo "You've won"
+		exit
+	fi
+}
+
+#Function to check for tie
+function conditionForTie()
+{
+	if (( count == 9 ))
+	then
+		echo "It's a tie"
+		exit
+	fi
+}
+
+#Function to check the winning condition 
+function winningCondition()
+{
+	for (( i=1; i<=9 ;i++ ))
+	do
+		winningConditionForRows
+		winningConditionForColumns
+		winningConditionForDiagonals
+		conditionForTie
+	done
+}
+
+#MAIN
+displayTheBoard
+play
